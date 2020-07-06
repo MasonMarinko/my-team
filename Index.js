edf863ba92963a1b49a56af1feb00f26c426b61f
MasonM <Mason.P.Marinko@gmail.com>
2020-07-06 15:26:52 -0600

    working on getting answers to different functions

--- Index.js
+++ Index.js
@@ -1,3 +1,56 @@
 const Employee = require('./lib/Employee');
+const inquirer = require('inquirer');
 
-new Employee().initializeEmployee();
+Employee.prototype.initializeEmployee = function () {
+    inquirer
+      .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+            }
+        }
+        },
+        {
+            type:'input',
+            name: 'id',
+            message: 'What is your employee number?',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid employee number!');
+                }
+            }
+        },
+        {
+            type:'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address!');
+                }
+            }
+        },        
+        ])     
+      // destructure name from the prompt object
+      .then(({
+        name, id, email
+      }) => {
+        // this.player = new Player(name);
+        console.log(name)
+        console.log(id)
+        console.log(email)
+        // this.startNewBattle();
+      });
+  };
+
+  new Employee().initializeEmployee();
